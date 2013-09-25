085c0ea79a8deb38e7969b5583f4963683889272
Robbert Hazenberg <robbert.hazenberg@gmail.com>
2013-09-25 17:20:12 +0200

    t
    t

--- CMDA1213-Fed2/Les 4/Lesopdrachten/mvc5/js/app/collections/leagueCollection.js
+++
@@ -1,8 +0,0 @@
-// define league, a collection of tournaments
-FED2.League = Backbone.Collection.extend({
-    model: FED2.Tournament,
-	
-	comparator : function(league) {
-		return league.get("name");
-	}
-});
